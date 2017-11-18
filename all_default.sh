65e20b156df7a93849bab2aefc8d6c1ecc7ce106
huang-zhongyi <790209714@qq.com>
2017-11-18 17:07:13 +0800

    add pytorh conda install to all_default.sh

--- all_default.sh
+++ all_default.sh
@@ -2,6 +2,7 @@ source activate tensorflow
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64/
 pip install -U pip
 pip install -r requirements.txt
+conda install pytorch torchvision cuda80 -c soumith
 bash ./vc_demo.sh vc_gan_test2 /home/lab-huang.zhongyi/data/cmu_arctic
 
 
