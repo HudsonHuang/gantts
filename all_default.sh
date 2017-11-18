905afb44aa7b2fdf1e7ef611e5e76b42e7aadf30
huang-zhongyi <790209714@qq.com>
2017-11-18 22:22:01 +0800

    finish all_default.sh, can run now!

--- all_default.sh
+++ all_default.sh
@@ -2,6 +2,8 @@ source activate tensorflow
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64/
 pip install -U pip
 #conda install pytorch torchvision cuda80 -c soumith
+pip install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp35-cp35m-manylinux1_x86_64.whl 
+pip install torchvision
 python setup.py install
 bash ./vc_demo.sh vc_gan_test2 /home/lab-huang.zhongyi/data/cmu_arctic
 
